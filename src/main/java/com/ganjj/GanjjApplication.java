261a25660f9a2fc50ce0f218412ed190172a0bf3
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-09-09 19:11:12 -0300

    Initial commit

---
+++ src/main/java/com/ganjj/GanjjApplication.java
@@ -0,0 +1,13 @@
+package com.ganjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanjjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanjjApplication.class, args);
+	}
+
+}
